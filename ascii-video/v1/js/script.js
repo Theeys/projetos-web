a76a1b45c57ed2743959b133ca511efad842d84b
Matheus Gomes <theysstudios@gmail.com>
2023-11-17 17:08:44 -0300

    Subindo arquivos da branch developer

--- ascii-video/v1/js/script.js
+++ ascii-video/v1/js/script.js
@@ -1,1 +1,143 @@
-!function(){let x=document.getElementById("video"),e=document.getElementById("text-video"),t=document.getElementById("canvas-video"),a=t.getContext("2d"),d="\xc6\xd1\xcaŒ\xd8M\xc9\xcb\xc8\xc3\xc2WQB\xc5\xe6#N\xc1\xfeE\xc4\xc0HKRŽœXg\xd0\xeaq\xdbŠ\xd5\xd4A€\xdfpm\xe3\xe2G\xb6\xf8\xf0\xe98\xda\xdc$\xebd\xd9\xfd\xe8\xd3\xde\xd6\xe5\xff\xd2b\xa5FD\xf1\xe1ZP\xe4š\xc7\xe0h\xfb\xa7\xddkŸ\xaeS9žUTe6\xb5Oyx\xce\xbef4\xf55\xf4\xfa&a\xfc™2\xf9\xe7w\xa9Y\xa30V\xcdL\xb13\xcf\xcc\xf3C@n\xf6\xf2s\xa2u‰\xbd\xbc‡zJƒ%\xa4Itoc\xeerjv1l\xed=\xef\xec<>i7†[\xbf?\xd7}*{+()/\xbb\xab•\xac|!\xa1\xf7\xa6\xaf—^\xaa„”“~\xb3\xba\xb2–\xb0\xad\xb9‹›;:’‘‚’˜ˆ\xb8…\xb7\xa8\xb4`".split("").reverse().join(""),c=d.replaceAll("_","\xa0"),$=["#9400D3","#4B0082","#0000FF",],l=()=>{let x=Math.floor(Math.random()*$.length);return $[x]},n=e=>{t.width=110,t.height=70,e.drawImage(x,0,0,110,70)},r=x=>{let e=x.getImageData(0,0,t.width,t.height),a=e.data,d=0,c=Array(70).fill(0).map(()=>[]);for(let $=0,l=0;$<a.length;$+=4){let n=(a[$]+a[$+1]+a[$+2])/3;c[d].push(n),l<110&&l++,110===l&&(l=0,d+=1)}return c},f=(x,e)=>{let d=a.getImageData(x,e,t.width,t.height),c=d.data;var $=c[0],l=c[1],n=c[2];return c[3],`${$}, ${l}, ${n}`},i=x=>{let e=Math.floor(x/255*(d.length-1));return c[e]},b=(x,e)=>{let t="";for(let a=0;a<e.length;a++){t+="<div>";for(let d=0;d<e[a].length;d++)t+=`<span style='color: rgb(${f(d,a)})'> ${i(e[a][d])} </span>`;t+="</div><br>"}x.innerHTML=t},o=!0;navigator.mediaDevices.getUserMedia({video:!0,audio:!1}).then(function(e){x.srcObject=e,x.play()}).catch(function(x){console.log("An error occurred: "+x)});let g=()=>requestAnimationFrame(()=>{n(a);let x=r(a);b(e,x),l(),o&&g()});g(),document.getElementById("start").addEventListener("click",x=>{o=!0,g()}),document.getElementById("stop").addEventListener("click",x=>{o=!1})}();
+(function () {
+    const video = document.getElementById('video')
+    const textVideo = document.getElementById('text-video')
+    const canvas = document.getElementById('canvas-video')
+    const ctx = canvas.getContext('2d');
+    const width = 220 / 2, height = 140 / 2;
+
+    const ASCII_PALETTES = {
+                            blockelement: "█",
+                            solidblock: "█",
+                            minimalist: "#+-.",
+                            mediumset: "@%#8*+=-:,.",
+                            longerset: "$@B%8&WM#*oa*+=-:,.",
+                            fullset: "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft?+~i!lI;:,^`'.",
+                            maxset: "\xc6\xd1\xcaŒ\xd8M\xc9\xcb\xc8\xc3\xc2WQB\xc5\xe6#N\xc1\xfeE\xc4\xc0HKRŽœXg\xd0\xeaq\xdbŠ\xd5\xd4A€\xdfpm\xe3\xe2G\xb6\xf8\xf0\xe98\xda\xdc$\xebd\xd9\xfd\xe8\xd3\xde\xd6\xe5\xff\xd2b\xa5FD\xf1\xe1ZP\xe4š\xc7\xe0h\xfb\xa7\xddkŸ\xaeS9žUTe6\xb5Oyx\xce\xbef4\xf55\xf4\xfa&a\xfc™2\xf9\xe7w\xa9Y\xa30V\xcdL\xb13\xcf\xcc\xf3C@n\xf6\xf2s\xa2u‰\xbd\xbc‡zJƒ%\xa4Itoc\xeerjv1l\xed=\xef\xec<>i7†[\xbf?\xd7}*{+()/\xbb\xab•\xac|!\xa1\xf7\xa6\xaf—^\xaa„”“~\xb3\xba\xb2–\xb0\xad\xb9‹›;:’‘‚’˜ˆ\xb8…\xb7\xa8\xb4`",
+                            numerical: "0896452317",
+                            extended: "@%#{}[]()<>^*+=~-:.",
+                            math: "π≠\xf7√=≈∞+\xd7-",
+                            arrow: "↑↗→↘↓↙←↖",
+                            alphabetic: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                            alphanumeric: "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz",
+                            default: "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0++++++++########"
+                        };
+
+    const gradient = ASCII_PALETTES["maxset"].split('').reverse().join('');
+    const preparedGradient = gradient.replaceAll('_', '\u00A0')
+    // const gradient = "_______++++++++########";
+    // const preparedGradient = gradient.replaceAll('_', '-')
+
+    const rainbowColors = [
+        '#9400D3',
+        '#4B0082',
+        '#0000FF',
+
+    ]
+
+    const randomRainbowColor = () => {
+        const i = Math.floor(Math.random() * rainbowColors.length)
+        return rainbowColors[i]
+    }
+
+    const init = () => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+            .then(function (stream) {
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(function (err) {
+                console.log("An error occurred: " + err);
+            });
+    }
+
+    const clearphoto = () => {
+        ctx.fillStyle = "#fff";
+        ctx.fillRect(0, 0, width, height);
+    }
+
+    const render = (ctx) => {
+        if (width && height) {
+            canvas.width = width;
+            canvas.height = height;
+            ctx.drawImage(video, 0, 0, width, height);
+        } else {
+            clearphoto();
+        }
+    }
+
+    const getPixelsGreyScale = (ctx) => {
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const data = imageData.data;
+        let row = 0
+        const res = new Array(height).fill(0).map(() => []);
+        for (let i = 0, c = 0; i < data.length; i += 4) {
+            const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+            let curr = res[row]
+            curr.push(avg)
+            if (c < width) {
+                c++
+            }
+            if (c === width) {
+                c = 0
+                row += 1
+            }
+
+        }
+        return res
+    }     
+
+    const getPixelColor = (x,y) => {
+        const imageData = ctx.getImageData(x, y, canvas.width, canvas.height);
+        const data = imageData.data;
+        let row = 0
+
+        var red = data[0];
+        var green = data[1];
+        var blue = data[2];
+        var alpha = data[3];
+
+        return `${red}, ${green}, ${blue}`
+    }             
+
+    const getCharByScale = (scale) => {
+        const val = Math.floor(scale / 255 * (gradient.length - 1))
+        return preparedGradient[val]
+    }
+
+    const renderText = (node, textDarkScale) => {
+        let txt = ``
+        for (let i = 0; i < textDarkScale.length; i++) {
+            txt += `<div>`
+            for (let k = 0; k < textDarkScale[i].length; k++) {
+                txt += `<span style='color: rgb(${getPixelColor(k,i)})'> ${getCharByScale(textDarkScale[i][k])} </span>`
+            }
+            txt += `</div><br>`
+        }
+        //txt += `</div>`
+        node.innerHTML = txt
+    }
+
+    let running = true;
+
+    init()
+    const frame = () => requestAnimationFrame(() => {
+        render(ctx)
+        const chars = getPixelsGreyScale(ctx)
+        renderText(textVideo, chars)
+        randomRainbowColor()
+        if (running)
+            frame()
+    })
+    frame()
+
+    
+    document.getElementById('start').addEventListener('click', (e) => {
+        running = true
+        frame()
+    })
+
+    document.getElementById('stop').addEventListener('click', (e) => {
+        running = false
+    })
+
+})()
--- ascii-video/v2/js/script.js
+++ ascii-video/v2/js/script.js
@@ -1,1 +1,149 @@
-!function(){let e=document.querySelector("video"),t=document.querySelector(".canvas-text-video"),x=document.querySelector(".canvas-video"),a=x.getContext("2d"),i=t.getContext("2d"),l=document.querySelector(".btn-switch-color-video"),d=document.querySelector(".btn-switch-video"),c=document.querySelector(".btn-switch-webcam"),n=document.querySelector(".btn-switch-paleta");i.font="12px monospace",i.imageSmoothingEnabled=!0,i.fontKerning="none",i.textAlign="start",i.textBaseline="top",i.textRendering="optimizeSpeed",i.fillStyle="white";let r={solidblock:"█",minimalist:"#+-.",mediumset:"@%#8*+=-:,.",longerset:"$@B%8&WM#*oa*+=-:,.",fullset:"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft?+~i!lI;:,^`'.",maxset:"\xc6\xd1\xcaŒ\xd8M\xc9\xcb\xc8\xc3\xc2WQB\xc5\xe6#N\xc1\xfeE\xc4\xc0HKRŽœXg\xd0\xeaq\xdbŠ\xd5\xd4A€\xdfpm\xe3\xe2G\xb6\xf8\xf0\xe98\xda\xdc$\xebd\xd9\xfd\xe8\xd3\xde\xd6\xe5\xff\xd2b\xa5FD\xf1\xe1ZP\xe4š\xc7\xe0h\xfb\xa7\xddkŸ\xaeS9žUTe6\xb5Oyx\xce\xbef4\xf55\xf4\xfa&a\xfc™2\xf9\xe7w\xa9Y\xa30V\xcdL\xb13\xcf\xcc\xf3C@n\xf6\xf2s\xa2u‰\xbd\xbc‡zJƒ%\xa4Itoc\xeerjv1l\xed=\xef\xec<>i7†[\xbf?\xd7}*{+()/\xbb\xab•\xac|!\xa1\xf7\xa6\xaf—^\xaa„”“~\xb3\xba\xb2–\xb0\xad\xb9‹›;:’‘‚’˜ˆ\xb8…\xb7\xa8\xb4`",numerical:"0896452317",extended:"@%#{}[]()<>^*+=~-:.",math:"π≠\xf7√=≈∞+\xd7-",arrow:"↑↗→↘↓↙←↖",alphabetic:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",alphanumeric:"ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz",default:"\xa0\xa0\xa0\xa0\xa0\xa0\xa0++++++++########"},s="mediumset",f=r[s].split("").reverse().join(""),o=e=>{let t=e.getImageData(0,0,x.width,x.height),a=t.data,i=0,l=Array(x.height).fill(0).map(()=>[]);for(let d=0,c=0;d<a.length;d+=4){let n=(a[d]+a[d+1]+a[d+2])/3;l[i].push(n),c<x.width&&c++,c===x.width&&(c=0,i+=1)}return l},$=(e,t)=>{let i=a.getImageData(e,t,x.width,x.height);return`${i.data[0]}, ${i.data[1]}, ${i.data[2]}, ${i.data[3]}`},h=e=>{let t=Math.floor(e/255*(f.length-1));return f[t]},b=()=>{i.clearRect(0,0,t.width,t.height);let e=o(a);for(let x=0;x<e.length;x++)for(let l=0;l<e[x].length;l++)m&&(i.fillStyle=`rgba(${$(l,x)})`),i.fillText(h(e[x][l]),10*l,10*x)},u=()=>requestAnimationFrame(()=>{a.drawImage(e,0,0,x.width,x.height),b(),g&&u()}),g=!0,m=!0,v=!0;navigator.mediaDevices.getUserMedia({video:!0,audio:!1}).then(function(t){e.srcObject=t,e.play()}).catch(function(e){console.log("Nao foi possivel obter acesso a camera: "+e)}),u(),c.addEventListener("click",t=>{(v=!v)?(c.innerHTML='<i class="fas fa-video"></i>',e.removeAttribute("hidden")):(c.innerHTML='<i class="fa-solid fa-video-slash"></i>',e.setAttribute("hidden",!0))}),d.addEventListener("click",e=>{(g=!g)?(d.innerHTML='<i class="fas fa-pause"></i>',u()):d.innerHTML='<i class="fas fa-play"></i>'}),l.addEventListener("click",e=>{(m=!m)?l.innerHTML='<i class="fas fa-tint"></i>':(l.innerHTML='<i class="fas fa-tint-slash"></i>',i.fillStyle="white")}),n.addEventListener("click",e=>{let t=Object.keys(r),x=t.indexOf(s);f=r[s=t[x=x<t.length-1?x+1:0]].split("").reverse().join("")})}();
+(function () {
+    const video = document.querySelector('video')
+    const canvasTxtVideo = document.querySelector('.canvas-text-video')
+    const canvas = document.querySelector('.canvas-video')
+    const ctx = canvas.getContext('2d');
+    const ctxTxtVideo = canvasTxtVideo.getContext('2d');
+    const btnSwitchColor = document.querySelector('.btn-switch-color-video')
+    const btnSwitchTextVideo = document.querySelector('.btn-switch-video')        
+    const btnSwitchWebcam = document.querySelector('.btn-switch-webcam')   
+    const btnSwitchPaleta = document.querySelector('.btn-switch-paleta')
+
+    ctxTxtVideo.font = `12px monospace`
+    ctxTxtVideo.imageSmoothingEnabled = !0
+    ctxTxtVideo.fontKerning = "none"
+    ctxTxtVideo.textAlign = "start"
+    ctxTxtVideo.textBaseline = "top"
+    ctxTxtVideo.textRendering = "optimizeSpeed"
+    ctxTxtVideo.fillStyle = "white"
+
+    const ASCII_PALETTES = {
+                            solidblock: "█",
+                            minimalist: "#+-.",
+                            mediumset: "@%#8*+=-:,.",
+                            longerset: "$@B%8&WM#*oa*+=-:,.",
+                            fullset: "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft?+~i!lI;:,^`'.",
+                            maxset: "\xc6\xd1\xcaŒ\xd8M\xc9\xcb\xc8\xc3\xc2WQB\xc5\xe6#N\xc1\xfeE\xc4\xc0HKRŽœXg\xd0\xeaq\xdbŠ\xd5\xd4A€\xdfpm\xe3\xe2G\xb6\xf8\xf0\xe98\xda\xdc$\xebd\xd9\xfd\xe8\xd3\xde\xd6\xe5\xff\xd2b\xa5FD\xf1\xe1ZP\xe4š\xc7\xe0h\xfb\xa7\xddkŸ\xaeS9žUTe6\xb5Oyx\xce\xbef4\xf55\xf4\xfa&a\xfc™2\xf9\xe7w\xa9Y\xa30V\xcdL\xb13\xcf\xcc\xf3C@n\xf6\xf2s\xa2u‰\xbd\xbc‡zJƒ%\xa4Itoc\xeerjv1l\xed=\xef\xec<>i7†[\xbf?\xd7}*{+()/\xbb\xab•\xac|!\xa1\xf7\xa6\xaf—^\xaa„”“~\xb3\xba\xb2–\xb0\xad\xb9‹›;:’‘‚’˜ˆ\xb8…\xb7\xa8\xb4`",
+                            numerical: "0896452317",
+                            extended: "@%#{}[]()<>^*+=~-:.",
+                            math: "π≠\xf7√=≈∞+\xd7-",
+                            arrow: "↑↗→↘↓↙←↖",
+                            alphabetic: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                            alphanumeric: "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz",
+                            default: "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0++++++++########"
+                        };
+    let optionGradient = "mediumset"
+    let gradient = ASCII_PALETTES[optionGradient].split('').reverse().join('');
+
+    const init = () => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+            .then(function (stream) {
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(function (err) {
+                console.log("Nao foi possivel obter acesso a camera: " + err);
+            });
+    }
+
+    const getPixelsGreyScale = (ctx) => {
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const data = imageData.data;
+        let row = 0
+        const res = new Array(canvas.height).fill(0).map(() => []);
+        for (let i = 0, c = 0; i < data.length; i += 4) {
+            const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+            let curr = res[row]
+            curr.push(avg)
+            if (c < canvas.width) {
+                c++
+            }
+            if (c === canvas.width) {
+                c = 0
+                row += 1
+            }
+
+        }
+        return res
+    }     
+
+    const getPixelColor = (x,y) => {
+        const imageData = ctx.getImageData(x, y, canvas.width, canvas.height);
+        const data = imageData.data;
+
+        var red = data[0];
+        var green = data[1];
+        var blue = data[2];
+        var alpha = data[3];
+
+        return `${red}, ${green}, ${blue}, ${alpha}`
+    }             
+
+    const getCharByScale = (scale) => {
+        const val = Math.floor(scale / 255 * (gradient.length - 1))
+        return gradient[val]
+    }
+
+    const renderText = () => {
+        ctxTxtVideo.clearRect(0, 0, canvasTxtVideo.width, canvasTxtVideo.height);
+        const chars = getPixelsGreyScale(ctx)
+        for (let i = 0; i < chars.length; i++) {
+            for (let k = 0; k < chars[i].length; k++) {
+                if(color) ctxTxtVideo.fillStyle = `rgba(${getPixelColor(k,i)})`
+                ctxTxtVideo.fillText(getCharByScale(chars[i][k]), k*10, i*10)
+            }
+        }
+    }
+
+    const frame = () => requestAnimationFrame(() => {
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        renderText()
+        if (running)
+            frame()
+    })
+
+    let running = true;
+    let color = true;
+    let webcam = true;
+    init()
+    frame()
+
+    btnSwitchWebcam.addEventListener('click', (e) => {
+        webcam = !webcam
+        if(webcam){
+            btnSwitchWebcam.innerHTML = '<i class="fas fa-video"></i>'
+            video.removeAttribute('hidden')
+        } else {
+            btnSwitchWebcam.innerHTML = '<i class="fa-solid fa-video-slash"></i>'
+            video.setAttribute('hidden', true)
+        }    
+    })
+    
+    btnSwitchTextVideo.addEventListener('click', (e) => {
+        running = !running
+        if(running){
+            btnSwitchTextVideo.innerHTML = '<i class="fas fa-pause"></i>'
+            frame()
+        } else {
+            btnSwitchTextVideo.innerHTML = '<i class="fas fa-play"></i>'
+        }
+    })
+
+    btnSwitchColor.addEventListener('click', (e) => {
+        color = !color
+        if(color){
+            btnSwitchColor.innerHTML = '<i class="fas fa-tint"></i>'
+        } else {
+            btnSwitchColor.innerHTML = '<i class="fas fa-tint-slash"></i>'
+            ctxTxtVideo.fillStyle = "white"
+        }            
+    })
+
+    btnSwitchPaleta.addEventListener('click', (e) => {
+        const chaves = Object.keys(ASCII_PALETTES);
+        let index = chaves.indexOf(optionGradient)
+        index = index < (chaves.length-1)? index+1 : 0
+        optionGradient = chaves[index]
+        gradient = ASCII_PALETTES[optionGradient].split('').reverse().join('');        
+    })
+})()
