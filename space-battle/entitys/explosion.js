2d27cc5cf86c1cd7fa8d8dbd1213efcadd935615
Matheus Gomes <theysstudios@gmail.com>
2024-11-23 11:25:22 -0300

    Adicionado efeito sonoro

---
+++ space-battle/entitys/explosion.js
@@ -0,0 +1,15 @@
+import { Entity } from "./entity.js"
+
+export class Explosion extends Entity {
+    constructor(pos, size) {
+        super(pos, size)
+    }
+
+    draw(ctx) {
+        super.draw(ctx)
+    }
+
+    update(game) {
+        super.update(game)
+    }
+}
